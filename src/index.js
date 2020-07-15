46ae3e4171d2214646e8c365fa7355a834177135
varekaiwork <serhiisaviukwork@gmail.com>
2020-07-15 16:52:12 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './components/app/';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.state = {
+      years: 26,
+    };
+  }
+  nextYear() {
+    console.log(1);
+    // this.state.years++;
+    this.setState;
+  }
+  render() {
+    const { name, surname, link } = this.props;
+    const { years } = this.state;
+    return (
+      <>
+        <button onClick={this.nextYear}>++</button>
+        <h1>
+          My name is {name}, surname - {surname}, years ={years}
+        </h1>
+        <a href={link}>My profile</a>
+      </>
+    );
+  }
+}
+
+const All = () => {
+  return (
+    <>
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Don" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Baton" surname="Smith" link="facebook.com" />
+    </>
+  );
+};
+
+ReactDOM.render(<All />, document.getElementById('root'));
